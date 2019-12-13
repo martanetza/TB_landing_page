4ba02a3e1aee8fe389e9d9f1959940bbc84d83d5
martanetza <39929716+martanetza@users.noreply.github.com>
2019-12-13 18:37:02 +0100

    testimonial module order

--- wp-content/themes/TB_landing_page/templates/content_modules/testimonial_module.php
+++ wp-content/themes/TB_landing_page/templates/content_modules/testimonial_module.php
@@ -6,6 +6,15 @@
     $author_job_description = get_sub_field('author_job_description');
     $image = get_sub_field('image');
     $yellow_line_visible = get_sub_field('yellow_line_visible');
+    $image_placement = get_sub_field('image_placement');
+    if ($image_placement == 'Left'):
+        $order_image = "order-1";
+        $order_text = 'order-2';
+         else:
+         $order_image = "order-2";
+        $order_text = 'order-1';
+        endif;
+
     if ($yellow_line_visible == 0):
         $yellow_line = "yellow-line-unvisible";
     else:
@@ -20,12 +29,14 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 d-flex align-items-center">
+        <div
+            class="col-xs-12 col-sm-12 col-md-6 col-lg-6 d-flex align-items-center <?php echo $order_image; ?>">
             <img src="<?php echo $image['sizes']['large']; ?>"
                 class="testimonialImage">
         </div>
 
-        <div class="col-xs-11 col-sm-11 col-md-6 col-lg-6 ">
+        <div
+            class="col-xs-11 col-sm-11 col-md-6 col-lg-6 <?php echo $order_text; ?> ">
             <p class="testimonial-txt">
                 <?php echo $testimonial_text; ?>
             </p>
@@ -34,7 +45,6 @@
                 <span class="autorJobdescription"><?php echo $author_job_description; ?></span>
             </p>
         </div>
-        <div class="col-xs-1 col-sm-1 col-md-1  col-lg-1"></div>
     </div>
     <div class="yellow-line <?php echo $yellow_line; ?>"></div>
 </section>
